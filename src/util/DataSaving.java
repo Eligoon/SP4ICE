cb683eaf464636d5a38920e2c390e9dbe2b771a1
Eligoon <derpepind117@live.dk>
2025-12-08 12:55:19 +0100

    added comments

--- src/util/DataSaving.java
+++ src/util/DataSaving.java
@@ -2,9 +2,10 @@ package util;
 import java.sql.*;
 
 public class DataSaving {
-
+    // Connection to database
     Connection connection;
 
+    // Method to establish connection to our database
     public void connect(String url) {
         try {
             connection = DriverManager.getConnection(url);
