e24d10b04b94d86bacb449489bcb03680b5dcf07
KristinaBengtsson <cph-kd104@stud.ek.dk>
2025-12-08 12:44:54 +0100

    Creature method signatures

--- src/creatures/Creature.java
+++ src/creatures/Creature.java
@@ -1,4 +1,50 @@
 package creatures;
 
+import collectibles.Item;
+import creatures.attributes.Race;
+import creatures.attributes.Stats;
+import world.Location;
+
+/* The Creature class represents any living being in the game.
+* This is a superclass for Player and NPC. */
 public class Creature {
+    protected String name; //The creatures display name
+    protected Race race; // The creatures tace (Human, Elf, Orc, Dwarf)
+    protected Stats stats; // Thea creatures stats (health, strength, dexteryty and intelligence)
+
+// --- Constructur for creating a Creature ---
+    public Creature(String name, Race race, Stats stats) {
+
+        this.name = name;
+        this.race = race;
+        this.stats = stats;
+    }
+
+    public void interact(){ // Allows creature to interact with something in the game world.
+         //TODO
+    }
+    public void fight (Creature target) { //Handles combat with another creature
+         //TODO
+    }
+    public void move (Location location) { // Moves the creature to another location
+         //TODO
+    }
+
+    public void pickUpItem (Item item) { // Adds a item to the creatures inventory (for Player)
+        //TODO
+    }
+
+// --- Getters ---
+    public String getName () {
+        return name;
+    }
+
+    public Race getRace () {
+        return race;
+    }
+
+    public Stats getStats () {
+        return stats;
+    }
+
 }
