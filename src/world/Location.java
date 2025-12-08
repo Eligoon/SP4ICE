3528bc649ce18f82ad1a777d0d022e96a1790cac
LoveBytes25 <keramsby@gmail.com>
2025-12-08 11:36:38 +0100

    Updated Location class with instance variables

--- src/world/Location.java
+++ src/world/Location.java
@@ -1,4 +1,17 @@
 package world;
 
+import collectibles.Item;
+import controller.Choice;
+import creatures.Creature;
+import java.util.List;
+
 public class Location {
+    private String locationName;
+    private String description;
+    private List<Creature> creatures;
+    private List<Item> items;
+    private List<Choice> availableChoices;
+
+
+
 }
