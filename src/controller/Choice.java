a3320e5feae5652a746d2edb5f7325edcbede71d
Eligoon <derpepind117@live.dk>
2025-12-10 13:48:00 +0100

    created choice class to help the player take choices througout the game

--- src/controller/Choice.java
+++ src/controller/Choice.java
@@ -1,4 +1,16 @@
 package controller;
 
+import world.Location;
+
 public class Choice {
+
+    private String description;
+    private Location targetLocation;
+    private requirements (need datatype)
+
+    public Choice(String description, Location targetLocation)
+    {
+        this.description = description;
+        this.targetLocation = targetLocation;
+    }
 }
