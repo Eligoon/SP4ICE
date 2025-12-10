0ffc37457fe4e7a40252853a40fe817340e870e4
Eligoon <derpepind117@live.dk>
2025-12-10 11:53:34 +0100

    potential ItemRegistry class

--- src/util/ItemRegistry.java
+++ src/util/ItemRegistry.java
@@ -1,5 +1,7 @@
 package util;
 
+import collectibles.Item;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,9 +20,9 @@ public class ItemRegistry {
     public static Item create(String itemName) {
         Item prototype = registry.get(itemName);
         if (prototype != null) {
-            return prototype.clone(); // clone to avoid sharing mutable state
+            return prototype.clone(); // clone to avoid sharing mutable state (part of java class Object)
         } else {
-            System.err.println("WARNING: Unknown item '" + itemName + "' requested from ItemRegistry.");
+            System.err.println("ERROR: Unknown item " + itemName + " requested from ItemRegistry.");
             return null;
         }
     }
