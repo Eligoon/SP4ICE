fddf63f1f37fb98273cf0dbdda686655db7d4b2c
Eligoon <derpepind117@live.dk>
2025-12-10 11:49:08 +0100

    potential ItemRegistry class

---
+++ src/util/ItemRegistry.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ItemRegistry {
+
+    // Registry map storing prototype items by name
+    private static Map<String, Item> registry = new HashMap<>();
+
+    // Initialize registry with all items in the game
+    // run this once when the class is loaded, before anything else
+    static {
+        // Items will be added here later
+    }
+
+    // Create a new instance of an item by name
+    public static Item create(String itemName) {
+        Item prototype = registry.get(itemName);
+        if (prototype != null) {
+            return prototype.clone(); // clone to avoid sharing mutable state
+        } else {
+            System.err.println("WARNING: Unknown item '" + itemName + "' requested from ItemRegistry.");
+            return null;
+        }
+    }
+}
