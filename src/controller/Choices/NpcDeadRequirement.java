f441f2062b10f172d4531e0ec764f85c251c5672
Eligoon <derpepind117@live.dk>
2025-12-11 11:38:47 +0100

    NpcDeadRequirement class

---
+++ src/controller/Choices/NpcDeadRequirement.java
@@ -0,0 +1,18 @@
+package controller.requirements;
+
+import creatures.NPC;
+import creatures.Player;
+
+public class NpcDeadRequirement implements Requirement {
+
+    private NPC npc; // The NPC that must be dead
+
+    public NpcDeadRequirement(NPC npc) {
+        this.npc = npc;
+    }
+
+    @Override
+    public boolean isMet(Player player) {
+        return npc.isDead();
+    }
+}
