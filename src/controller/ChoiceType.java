ccc4b1c561b8b31a3b4287f851a4cdc058d9fed6
Eligoon <derpepind117@live.dk>
2025-12-11 11:22:08 +0100

    Enum added with choicetypes / options

---
+++ src/controller/ChoiceType.java
@@ -0,0 +1,10 @@
+package controller;
+
+// This is a list of the different types of actions a Choice can be
+public enum ChoiceType {
+    MOVE,       // Go to another location
+    COMBAT,     // Start a fight
+    INTERACT,   // Use something or talk to something
+    USE_OBJECT, // Use an item
+    // can add more here
+}
