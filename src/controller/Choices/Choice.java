06ebeec2d22dd2be241b909f6b7a5912c5f206af
Eligoon <derpepind117@live.dk>
2025-12-11 11:47:26 +0100

    start of choice class with two contructors

--- src/controller/Choices/Choice.java
+++ src/controller/Choices/Choice.java
@@ -1,16 +1,38 @@
 package controller.Choices;
 
+import creatures.Creature;
+import creatures.Player;
 import world.Location;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Choice {
 
-    private String description;
-    private Location targetLocation;
-    private requirements (need datatype)
+    private String description;        // Text shown to the player
 
-    public Choice(String description, Location targetLocation)
-    {
+    private ChoiceType type;           // What the choice does (move, combat, etc.) (Enum)
+
+    private Location targetLocation;   // For MOVE choices
+    private Creature enemy;            // For COMBAT choices
+
+    private List<Requirement> requirements = new ArrayList<>();
+    // Requirements: item, npc dead, race, class
+
+    // Constructors below for choices
+
+    // Movement choice
+    public Choice(String description, Location targetLocation) {
         this.description = description;
         this.targetLocation = targetLocation;
+        this.type = ChoiceType.MOVE;
     }
+
+    // Combat choice
+    public Choice(String description, Creature enemy) {
+        this.description = description;
+        this.enemy = enemy;
+        this.type = ChoiceType.COMBAT;
+    }
+
 }
